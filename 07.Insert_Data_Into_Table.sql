7624b9469cd0179a6cd94a37571746e32e25289f
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2026-05-21 18:33:35 +0700

    postgres_exercise

---
+++ 07.Insert_Data_Into_Table.sql
@@ -0,0 +1,8 @@
+INSERT INTO employees 
+(first_name, last_name, hiring_date, salary, device_number)
+VALUES
+('Ivan', 'Petrov', '2026-01-01', 2500.50, 101),
+('Maria', 'Georgieva', '2026-02-15', 3200.75, 102),
+('Georgi', 'Ivanov', '2026-03-10', 2800.00, 103);
+
+SELECT * FROM employees;
